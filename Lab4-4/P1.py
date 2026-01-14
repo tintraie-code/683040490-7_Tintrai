1415ae5057c36af02a7a8e82ecc77bdc7a2f184e
tintraie-code <tintrai.e@kkumail.com>
2026-01-14 23:11:15 +0700

    Update 4-4

---
+++ Lab4-4/P1.py
@@ -0,0 +1,15 @@
+import room
+if __name__ == "__main__":
+    my_bedroom = room.Bedroom(11, 11, 6.3)
+    print(my_bedroom.describe_room())
+    print(my_bedroom.get_purpose())
+    print(f" The recommened light in the room is {my_bedroom.get_recommended_lighting()} lumens per square foot")
+   
+    print()
+    
+    my_kitchen = room.Kitchen(11, 12, True)
+    print(my_kitchen.describe_room())
+    print(my_kitchen.get_purpose())
+    print(f"The recommened light in the room is {my_kitchen.get_recommended_lighting()}")
+    counter,wall = my_kitchen.calculate_counter_space()
+    print(f"{counter} {wall}")
---
+++ Lab4-4/room.py
@@ -0,0 +1,50 @@
+from abc import ABC, abstractmethod
+
+class Room(ABC):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    
+    @abstractmethod
+    def get_purpose(self):
+        """Returns a string describing purposes of the room"""
+        pass
+
+    @abstractmethod
+    def get_recommended_lighting(self):
+        """Returns recommended lighting in lumens per square foot"""
+        pass
+
+    def calculate_area(self):
+        return self.length * self.width
+    
+    def describe_room(self):
+        area = self.calculate_area()
+        return f"A {self.__class__.__name__} of {area} sq ft used for {self.get_purpose()}"
+
+class Bedroom(Room):
+    def __init__(self, length, width, bed_size):
+        super().__init__(length, width)
+        self.bed_size = bed_size
+    def get_purpose(self):
+        return f"I am sleeping here on a {self.bed_size} ft bed"
+    def get_recommended_lighting(self):
+        return 15
+class Kitchen(Room):
+    def __init__(self, length, width, has_island = True):
+        super().__init__(length, width)
+        self.has_island = has_island
+    def get_purpose(self):
+        return "I make food and enjoy cooking here"
+    def get_recommended_lighting(self):
+        return 75
+     
+    def calculate_counter_space(self):
+        area = self.calculate_area()
+        if self.has_island:
+            self.island_counter_area = area / 5
+            self.wall_counter_area = area / 4
+        else:
+            self.island_counter_area = 0
+            self.wall_counter_area = area / 2
+        return self.island_counter_area , self.wall_counter_area
